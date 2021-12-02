ed6342841bc049766c6a463e6916c6796ea31a1d
Machete <51382106+IntoTheVortex@users.noreply.github.com>
2021-12-02 17:02:08 +0000

    working, wrong answer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def main():
+def aoc1():
   file = open('input.txt','r')
   input = True
   last = 0
@@ -10,7 +10,6 @@ def main():
       input = False
     else:
       new_number = int(line)
-      print("Line:",line, "new_number =", new_number, "last =", last)
       if new_number > last:
         count+=1
       last = new_number
@@ -19,15 +18,35 @@ def main():
   print("Count = ", count)
 
 
+def aoc2():
+  file = open('input.txt','r')
+  input = True
+  last_total = 0
+  last_num1 = 0
+  last_num2 = 0
+  count = 0
 
+  while(input):
+    line = file.readline()
+    if not line:
+      input = False
+    else:
+      new_number = int(line)
 
+      new_total = new_number + last_num1 + last_num2
+      if new_total > last_total:
+        count+=1
+      
+      last_total = new_total
+      last_num1 = last_num2
+      last_num2 = new_number
 
-
-
-
+  count -=1
+  print("Count = ", count)
 
 
 
 
 if __name__ == '__main__':
-  main()
+  #aoc1()
+  aoc2()
