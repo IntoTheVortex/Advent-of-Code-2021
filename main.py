6e45e6a7332b642c1465fcddf0bf988091b25413
matchete <>
2021-12-02 16:31:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+
+
+def main():
+  file = open('input.txt','r')
+
+  input = True
+  while(input):
+    line = file.readline()
+    if not line:
+      input = False
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  main()
