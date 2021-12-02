72cdd65c57894d29a60917b572cc11d62fd61b29
Machete <51382106+IntoTheVortex@users.noreply.github.com>
2021-12-02 16:41:59 +0000

    found correct answer

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-
-
-
-
 def main():
   file = open('input.txt','r')
-
   input = True
+  last = 0
+  count = 0
+
   while(input):
     line = file.readline()
     if not line:
       input = False
+    else:
+      new_number = int(line)
+      print("Line:",line, "new_number =", new_number, "last =", last)
+      if new_number > last:
+        count+=1
+      last = new_number
+
+  count -=1
+  print("Count = ", count)
 
 
 
